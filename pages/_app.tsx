02e87980eeaff36baf8224aef783c5d24dcea940
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-25 23:36:49 +0900

    add component directory & add Header, Footer component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../src/component/header";
+import Footer from "../src/component/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
