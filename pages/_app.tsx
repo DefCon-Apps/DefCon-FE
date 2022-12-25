f1db6d538c3c06ba57b58fed6ea6fa6675d268c9
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-26 00:03:07 +0900

    [Updated _app.tsx] : 함수형 컴포넌트 선언방식 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import Header from "../src/component/header";
 import Footer from "../src/component/Footer";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <div>
       <Header />
       <Component {...pageProps} />
       <Footer />
     </div>
-  );
-}
+  )
+
+};
+
+export default App;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <>
