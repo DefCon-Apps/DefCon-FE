dd4cee9dae6effa27ae6a2643b28ed8297ce414f
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-27 00:41:09 +0900

    Updated _document.tsx : Setting for Styled Components in Next.js Project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+// Import styled components ServerStyleSheet
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+    const originalRenderpage = ctx.renderPage;
+
+    try {
+      // styled components SSR 하는 부분
+      // Step 2: Retrieve styles from components in the page
+      ctx.renderPage = () =>
+        originalRenderpage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
