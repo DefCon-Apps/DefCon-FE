c9cc5d551998ce5e535eb32c36ba7682008a3195
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-27 00:38:12 +0900

    Add .babelrc for Styled-Components in Next.js Project

--- src/Common/Header.tsx
+++ src/Common/Header.tsx
@@ -1,11 +1,29 @@
 import Link from "next/link";
+import styled from "styled-components";
+
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <HeaderDiv>
+      <HeaderNavUl>
+        <li><Link href="/Designs">Designs </Link></li>
+        <li><Link href="/Projects">Projects</Link></li>
+        <li><Link href="/Members">Members</Link></li>
+      </HeaderNavUl>
+    </HeaderDiv>
   );
 };
+
+const HeaderDiv = styled.div`
+  width: 100%;
+  height: 5rem;
+  font-size: 20px;
+  background-color: red;
+`
+
+const HeaderNavUl = styled.ul`
+  display: flex;
+  justify-content: center;
+`
 
 export default Header;
