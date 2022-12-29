8999c17229e4fc5dd68258458e5ab2b8884707af
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-30 00:45:32 +0900

    Updated MainWorks.tsx : Add Title

--- pages/Main/MainWorks.tsx
+++ pages/Main/MainWorks.tsx
@@ -1,57 +1,80 @@
 import styled from "styled-components";
-import { ScrollMenu } from 'react-horizontal-scrolling-menu';
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
 import WorksCard from "../../src/Common/WorksCard";
 
 const dummyArray = [
   {
-		id : 'w1',
-  	text: 'Android',
-		image: 'red'
+    id: "w1",
+    text: "Android",
+    image: "red",
   },
-	{
-		id : 'w2',
-  	text: 'iOS',
-		image: 'blue'
+  {
+    id: "w2",
+    text: "iOS",
+    image: "blue",
   },
-	{
-		id : 'w3',
-  	text: 'Self-Repair',
-		image: 'grey'
+  {
+    id: "w3",
+    text: "Self-Repair",
+    image: "grey",
   },
-	{
-		id : 'w4',
-  	text: 'Web',
-		image: 'green'
+  {
+    id: "w4",
+    text: "Web",
+    image: "green",
   },
-	{
-		id : 'w5',
-  	text: 'Chat Bot',
-		image: 'yellow',
-  }
+  {
+    id: "w5",
+    text: "Chat Bot",
+    image: "yellow",
+  },
 ];
 
 const MainWorks = () => {
-    return(
-        <MainWorksWrapper>
-					<h1>여기다가 가로스크롤 구현하기!!!</h1>
-					<ScrollMenu>
-						{dummyArray.map((items) => (<WorksCard key={items.id} text={items.text} image={items.image} />))}
-					</ScrollMenu>
-        </MainWorksWrapper>
-    );
+  return (
+    <MainWorksWrapper>
+      <WorksTitle>
+        <h1>우리가 즐겨온 일들</h1>
+        <p>
+          우리 DEF:CON이 관심을 갖고 즐겨온 일들의 카테고리입니다.<br />
+          소프트웨어가 사용될 수 있다면 우리는 뭐든 재미있게 갖고 놀 수
+          있습니다.<br />
+        </p>
+      </WorksTitle>
+      <ScrollWrapper>
+        <ScrollMenu>
+          {dummyArray.map((items) => (
+            <WorksCard key={items.id} text={items.text} image={items.image} />
+          ))}
+        </ScrollMenu>
+      </ScrollWrapper>
+    </MainWorksWrapper>
+  );
 };
 
 const MainWorksWrapper = styled.div`
-	height: 100vh;
-
+  height: 100vh;
 `;
 
-const Test = styled.div`
-	width: 540px;
-	height: 400px;
-	background-color: red;
-	border-radius: 20px;
-	margin-right: 20px;
+const WorksTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+	margin: 0rem 0rem 5rem 22rem;
+
+	h1 {
+    text-align: left;
+    font-size: 4rem;
+    letter-spacing: -5px;
+  }
+
+  p {
+    margin-top: 10px;
+    font-size: 1.5rem;
+    font-weight: 300;
+  }
+`;
+
+const ScrollWrapper = styled.div`
 `;
 
 export default MainWorks;
