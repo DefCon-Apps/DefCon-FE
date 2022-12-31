cf110a9669bb6db8f8bde09fd3753f2c0e26de51
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-31 21:19:07 +0900

    Updated MainRectangle.tsx : Make array for each props

--- pages/Main/MainRectangle.tsx
+++ pages/Main/MainRectangle.tsx
@@ -7,9 +7,69 @@ import BackgroundCard from "../../src/Common/BackgroundCard";
 //     translateY: string;
 //     type: 'bordered' | 'filled';
 
+const mainRectangle = [
+  {
+    id: "r1",
+    color: "#35B6F7",
+    height: "22rem",
+    tranX: "-78rem",
+    tranY: "7rem",
+    type: "bordered"
+  },
+  {
+    id: "r2",
+    color: "#C7E7FF",
+    height: "22rem",
+    tranX: "60rem",
+    tranY: "27rem",
+    type: "filled"
+  },
+  {
+    id: "r3",
+    color: "#35B6F7",
+    height: "22rem",
+    tranX: "61rem",
+    tranY: "76rem",
+    type: "filled"
+  },
+  {
+    id: "r4",
+    color: "#00658F",
+    height: "22rem",
+    tranX: "59rem",
+    tranY: "71rem",
+    type: "filled"
+  },
+  {
+    id: "r5",
+    color: "#00658F",
+    height: "22rem",
+    tranX: "-78rem",
+    tranY: "105.6rem",
+    type: "bordered"
+  },
+  {
+    id: "r6",
+    color: "#C7E7FF",
+    height: "22rem",
+    tranX: "-78rem",
+    tranY: "143rem",
+    type: "bordered"
+  },
+  {
+    id: "r7",
+    color: "#35B6F7",
+    height: "22rem",
+    tranX: "59rem",
+    tranY: "160rem",
+    type: "bordered"
+  },
+];
+
 const MainRectangle = () => {
   return (
     <>
+      {}
       <BackgroundCard
         color={"#35B6F7"}
         height={"22rem"}
