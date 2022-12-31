d299531cdaa8c85f12f3a1d26abaa553bdb6e75f
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-31 20:41:11 +0900

    Add MainRectangle.tsx

---
+++ pages/Main/MainRectangle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import BackgroundCard from "../../src/Common/BackgroundCard";
+
+const MainRectangle = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default MainRectangle;
