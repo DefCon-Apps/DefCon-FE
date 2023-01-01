6cd207f31b4f0418f22babf3a0cc15a9380be8ba
yymin1022 <yymin1022@gmail.com>
2023-01-01 17:42:01 +0900

    Update MembersTitle : Adjust Base Layout

--- pages/Members/MembersTitle.tsx
+++ pages/Members/MembersTitle.tsx
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+
 const MembersTitle = () => {
     return (
-        <>
-            Members Page
-        </>
+        <MembersTitleContainer>
+            <MembersTitleMain>MEMBERS TITLE MAIN</MembersTitleMain>
+            <MembersTitleSub>MEMBERS TITLE SUB</MembersTitleSub>
+        </MembersTitleContainer>
     );
 };
+
+const MembersTitleContainer = styled.div`
+  width: 100%;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+const MembersTitleMain = styled.p`
+
+`;
+
+const MembersTitleSub = styled.p`
+
+`;
+
 
 export default MembersTitle;
