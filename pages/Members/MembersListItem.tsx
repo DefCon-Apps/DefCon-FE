23e14b32e75dfc3cf6f119fb40f283cc22de8ff0
yymin1022 <yymin1022@gmail.com>
2023-01-01 18:13:37 +0900

    Update MembersListItem : Added Data Interface for Props

--- pages/Members/MembersListItem.tsx
+++ pages/Members/MembersListItem.tsx
@@ -1,4 +1,18 @@
-const MembersListItem = () => {
+interface Props{
+    comment: string,
+    company: string,
+    id: string,
+    image: string,
+    name: string
+    sns: Array<snsData>
+}
+
+interface snsData{
+    isEnabled: boolean,
+    url: string
+}
+
+const MembersListItem = (props: Props) => {
     return (
         <div>
             Member Item
