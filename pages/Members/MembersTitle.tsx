daec70ea52d11462036d20b92b27dd1668fb9be1
yymin1022 <yymin1022@gmail.com>
2023-01-01 17:52:50 +0900

    Update MembersTitle : Adjust TitleMain Layout

--- pages/Members/MembersTitle.tsx
+++ pages/Members/MembersTitle.tsx
@@ -3,23 +3,25 @@ import styled from "styled-components";
 const MembersTitle = () => {
     return (
         <MembersTitleContainer>
-            <MembersTitleMain>MEMBERS TITLE MAIN</MembersTitleMain>
-            <MembersTitleSub>MEMBERS TITLE SUB</MembersTitleSub>
+            <MembersTitleMain>MEMBERS<br/>OF DEF:CON</MembersTitleMain>
+            <MembersTitleSub>DEF:CON은 성보고등학교 동창 네 명으로 구성된 팀입니다</MembersTitleSub>
         </MembersTitleContainer>
     );
 };
 
 const MembersTitleContainer = styled.div`
-  width: 100%;
-  
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+      width: 100%;
+      
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
 `;
 
 const MembersTitleMain = styled.p`
-
+    font-size: 45pt;
+    font-weight: 800;
+    line-height: 90px;
 `;
 
 const MembersTitleSub = styled.p`
