046a0225c1e054f2c535c1c3bf208ed7f1ea64ad
rdg1029 <wcle3456@gmail.com>
2023-01-01 15:39:51 +0900

    Add ProjectSection.tsx : Init

---
+++ pages/Projects/ProjectSection.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+interface Props {
+  name: string;
+  description: string;
+  children?: React.ReactNode;
+}
+
+const ProjectSection = (props: Props) => {
+  return (
+    <SectionWrapper>
+      <div>
+        <h1>{props.name}</h1>
+        <p>{props.description}</p>
+        {props.children}
+      </div>
+    </SectionWrapper>
+  )
+}
+
+const SectionWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  margin-left: 5rem;
+  
+  h1 {
+    text-align: left;
+    font-size: 55pt;
+  }
+
+  p {
+    margin: 1.5rem 0 1.5rem 0;
+    font-size: 18pt;
+    font-weight: 300;
+  }
+`;
+
+export default ProjectSection;
