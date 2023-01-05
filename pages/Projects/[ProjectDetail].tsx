8889ea7dabbe5dab4f912c1059574a7cf2f38a12
rdg1029 <wcle3456@gmail.com>
2023-01-05 10:56:30 +0900

    Update ProjectDetail.tsx : Apply image slider

--- pages/Projects/[ProjectDetail].tsx
+++ pages/Projects/[ProjectDetail].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import styled from "styled-components";
+import ImageSlider from "../../src/Common/ImageSlider";
 
 const ProjectDetail = ()=> {
   const router = useRouter();
@@ -13,7 +14,14 @@ const ProjectDetail = ()=> {
           <p>This is description about project id {projectId}.</p>
         </ProjectDetailTitle>
         <ProjectDetailContent>
-          <ProjectDetailImages>PROJECT_IMAGES</ProjectDetailImages>
+          <ImageSlider width="600px" height="450px"
+            images={[
+              "https://github.com/yymin1022/SeoulHealing/raw/master/Images/app_1.jpg",
+              "https://github.com/yymin1022/SeoulHealing/raw/master/Images/app_2.jpg",
+              "https://github.com/yymin1022/SeoulHealing/raw/master/Images/app_3.jpg",
+              "https://camo.githubusercontent.com/119f7acf538de712dafd8c7f50c562eff424432a569e7e847716fa701c406b6d/687474703a2f2f6d617a617373756d6e6964612e7774662f6170692f76322f67656e65726174655f62616467653f626f6a3d79796d696e31303232",
+              "https://camo.githubusercontent.com/501dce49fd773c15a1a59e8bd14109a465a175ffd39df3ff3136881398b479ba/687474703a2f2f6d617a617373756d6e6964612e7774662f6170692f76322f67656e65726174655f62616467653f626f6a3d63656f756e6a63",
+            ]} />
           <h1>Project description.</h1>
           <h2>Project description.</h2>
           <h3>Project description.</h3>
