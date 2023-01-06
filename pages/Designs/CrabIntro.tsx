0b4354d26c0203b2652b682c869d6d1253caabf5
HarenKei <seungmin1266@gmail.com>
2023-01-06 18:17:13 +0900

    Updated LogoIntro.tsx & CrabIntro.tsx : Designs page completed

--- pages/Designs/CrabIntro.tsx
+++ pages/Designs/CrabIntro.tsx
@@ -5,34 +5,48 @@ import BackgroundCard from "../../src/Common/BackgroundCard";
 const CrabIntro = () => {
   return (
     <CrabIntroWrapper>
-			<CrabIntroContents>
-				<CrabIntroDesc>
-					<p>
-            DEF:CON의 마스코트인 DEF:CON CRAB은 메인 컬러와 마찬가지로<br/>
-            Windows 10의 블루스크린에서 착안하였습니다.<br/>
-					  DEF:CON의 철자 C의 앞에 콜론 (:)을 삽입하여 블루스크린의 이모티콘인<br/>
-            &quot;:(&quot;을 연상할 수 있게 만들어냈죠.<br/><br/>
-
-					  코딩을 하다보면 웃음을 지을 때보단 울상일 때가 더 많은 우리의 <br/>
-					  표정과도 비슷한 DEF:CON CRAB은 DEF:CON의 초창기 로고부터 <br/>
-					  유지되어온 DEF:CON의 헤리티지입니다. <br/>
-					</p>
-				</CrabIntroDesc>
-				<CrabIntroTitle>
-					우리의 헤리티지<br/>
-					DEF:CON CRAB
-          <BackgroundCard color="#C7E7FF" height="22rem" translateX="1150px" translateY="-245px" type="filled"/>
-				</CrabIntroTitle>
-			</CrabIntroContents>
+      <CrabIntroContents>
+        <CrabIntroDesc>
+          <p>
+            DEF:CON의 마스코트인 DEF:CON CRAB은 메인 컬러와 마찬가지로
+            <br />
+            Windows 10의 블루스크린에서 착안하였습니다.
+            <br />
+            DEF:CON의 철자 C의 앞에 콜론 (:)을 삽입하여 블루스크린의 이모티콘인
+            <br />
+            &quot;:(&quot;을 연상할 수 있게 만들어냈죠.
+            <br />
+            <br />
+            코딩을 하다보면 웃음을 지을 때보단 울상일 때가 더 많은 우리의 <br />
+            표정과도 비슷한 DEF:CON CRAB은 DEF:CON의 초창기 로고부터 <br />
+            유지되어온 DEF:CON의 헤리티지입니다. <br />
+          </p>
+        </CrabIntroDesc>
+        <CrabIntroTitle>
+          우리의 헤리티지
+          <br />
+          DEF:CON CRAB
+        </CrabIntroTitle>
+        <BackgroundCard
+          color="#C7E7FF"
+          height="22rem"
+          translateX="1100px"
+          translateY="-108px"
+          type="filled"
+        />
+      </CrabIntroContents>
 
       <CrabIntroLogoWrapper>
         <LogoHistoryWrapper>
-        <Logo src="/Images/firstLogo.svg"/>
-        <Logo src="/Images/preLogo.svg"/>
-        <Logo src="/Images/mainLogo.svg"/>
+          <Logo src="/Images/firstLogo.svg" />
+          <Logo src="/Images/preLogo.svg" />
+          <Logo src="/Images/mainLogo.svg" />
         </LogoHistoryWrapper>
-        <Crab width={15} height={15} marginTop={8} />
-
+        <Crab 
+          width={15} 
+          height={15} 
+          marginTop={8} 
+        />
       </CrabIntroLogoWrapper>
     </CrabIntroWrapper>
   );
@@ -49,30 +63,27 @@ const CrabIntroWrapper = styled.div`
 `;
 
 const CrabIntroContents = styled.div`
-	display: flex;
-	flex-direction: row;
-	
+  display: flex;
+  flex-direction: row;
 `;
 
 const CrabIntroTitle = styled.h1`
-	font-size: 45pt;
-	letter-spacing: -5px;
+  font-size: 45pt;
+  letter-spacing: -5px;
 `;
 
 const CrabIntroDesc = styled.div`
   margin-right: 14vw;
-  p{
+  p {
     font-size: 16pt;
     font-weight: 200;
   }
 `;
 
-
 const CrabIntroLogoWrapper = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: 8vh;
-  
 `;
 
 const LogoHistoryWrapper = styled.div`
@@ -82,12 +93,10 @@ const LogoHistoryWrapper = styled.div`
   margin-right: 20vw;
 `;
 
-
 const Logo = styled.img`
   width: 20vw;
   margin-bottom: 8vh;
-  filter : drop-shadow(1px 1px 3px grey);
-  
+  filter: drop-shadow(1px 1px 3px grey);
 `;
 
 export default CrabIntro;
--- pages/Designs/LogoIntro.tsx
+++ pages/Designs/LogoIntro.tsx
@@ -1,12 +1,6 @@
 import styled from "styled-components";
 import BackgroundCard from "../../src/Common/BackgroundCard";
 
-// color: string;
-//   height: string;
-//   translateX: string;
-//   translateY: string;
-//   type: string;
-
 const LogoIntro = () => {
   return (
     <LogoIntroWrapper>
@@ -16,63 +10,66 @@ const LogoIntro = () => {
           <br />
           의미 있는 DEF:CON의 얼굴
         </LogoIntroTitle>
-				<LogoIntroImg src="/Images/mainLogo.svg" />
+        <LogoIntroImg src="/Images/mainLogo.svg" />
       </LogoIntroContents>
       <LogoIntroContext>
         <p>
-          우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.<br />
-          고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.<br />
-					로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고<br/>
-          &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를 입혀<br/>
-					단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
+          우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.
+          <br />
+          고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.
+          <br />
+          로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고
+          <br />
+          &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를 입혀
+          <br />
+          단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
         </p>
       </LogoIntroContext>
-			<BackgroundCard
-				color="#00658F"
-				height="22rem"
-				translateX="-1000px"
-				translateY="140px"
-				type="bordered"
-			/>
+      <BackgroundCard
+        color="#00658F"
+        height="22rem"
+        translateX="-1000px"
+        translateY="140px"
+        type="bordered"
+      />
     </LogoIntroWrapper>
   );
 };
 
 const LogoIntroWrapper = styled.div`
   height: 100vh;
-	margin-bottom: 10vh;
+  margin-bottom: 10vh;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-	font-family: "Noto Sans KR";
+  font-family: "Noto Sans KR";
 `;
 
 const LogoIntroContents = styled.div`
   display: flex;
   flex-direction: column;
-	justify-content: flex-start;
-	align-items: flex-start;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 
 const LogoIntroTitle = styled.h1`
   font-size: 45pt;
-	letter-spacing: -5px;
+  letter-spacing: -5px;
 `;
 
 const LogoIntroImg = styled.img`
   width: 25vw;
-	margin-top: 15vh;
+  margin-top: 15vh;
 `;
 
 const LogoIntroContext = styled.div`
-	margin-top: 30vh;
+  margin-top: 30vh;
   p {
     font-size: 16pt;
-		font-weight: 100;
-		text-align: right;
+    font-weight: 100;
+    text-align: right;
   }
 `;
-
 
 export default LogoIntro;
