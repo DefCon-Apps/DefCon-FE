c17452d1bb65c937a6e69b11febbdb78b31cf827
yymin1022 <yymin1022@gmail.com>
2023-01-07 04:13:11 +0900

    Update Member Page : Adjust Member Data Layout

--- pages/Members/MembersDetail.tsx
+++ pages/Members/MembersDetail.tsx
@@ -9,8 +9,15 @@ const MembersDetail = () => {
 };
 
 const MembersDetailContainer = styled.div`
-    height: 100%;
-    width: 100%;
+      height: 100%;
+      width: 100%;
+    
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+    
+      margin: 30px 0 100px 0;
 `;
 
 export default MembersDetail;
