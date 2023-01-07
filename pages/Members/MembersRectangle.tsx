b23ae33305374166942879689ad92ef6779e90d7
yymin1022 <yymin1022@gmail.com>
2023-01-08 04:02:24 +0900

    Update MembersRectangle : Added Base Attributes

--- pages/Members/MembersRectangle.tsx
+++ pages/Members/MembersRectangle.tsx
@@ -2,8 +2,13 @@ import BackgroundCard from "../../src/Common/BackgroundCard";
 
 const MainRectangle = () => {
     return (
-        <>
-            
-        </>
+        <BackgroundCard
+            key="MembersRectangle"
+            color="#35B6F7"
+            height="1200px"
+            translateX="200px"
+            translateY="200px"
+            type="bordered"
+        />
     );
 };
