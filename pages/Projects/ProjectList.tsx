2a639b74919bdc1cba742e6b375bfdb3e2832855
rdg1029 <wcle3456@gmail.com>
2023-01-22 08:34:20 +0900

    Update ProjectList.tsx : Fixed error about undefined map property at build time

--- pages/Projects/ProjectList.tsx
+++ pages/Projects/ProjectList.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ProjectList = (props: Props) => {
   return (
     <ScrollMenu>
-      {props.list.map((item, i) => {
+      {props.list && props.list.map((item, i) => {
         return <ProjectListItem key={i} id={item.id} image={item.data.image[0]} title={item.data.title} />
       })}
     </ScrollMenu>
