523b48634ed0ee1755e1825db305ab1571b5d0ff
HarenKei <seungmin1266@gmail.com>
2023-02-02 23:26:21 +0900

    Updated Header.tsx : Adjustmented Header Layout

--- src/Common/Header.tsx
+++ src/Common/Header.tsx
@@ -5,9 +5,7 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <HeaderDiv>
-      <HeaderLogoDiv>
-        <Link href="/"><img src="/Images/mainLogo.svg"/></Link>
-      </HeaderLogoDiv>
+        <Link href="/"><HeaderLogo src="/Images/mainLogo.svg"/></Link>
       <HeaderNavUl>
         <HeaderNavLi><Link href="/Designs">Designs </Link></HeaderNavLi>
         <HeaderNavLi><Link href="/Projects">Projects</Link></HeaderNavLi>
@@ -20,6 +18,7 @@ const Header = () => {
 const HeaderDiv = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 100vw;
   height: 8vh;
   position: fixed;
@@ -28,16 +27,14 @@ const HeaderDiv = styled.div`
   font-family: 'Noto Sans KR';
   font-size: 20px;
   font-weight: bold;
-
 `;
 
-
-const HeaderLogoDiv = styled.div`
-  width: 10rem;
-  height: 2rem;
+const HeaderLogo = styled.img`
+  width: 150px;
+  height: 30px;
   margin-left: 1.5rem;
   margin-top: 1.5rem;
-`
+`;
 
 const HeaderNavUl = styled.ul`
   display: flex;
@@ -45,13 +42,14 @@ const HeaderNavUl = styled.ul`
   
   margin-top: 2.5rem;
   margin-right: 2rem;
-`
+`;
 
 const HeaderNavLi = styled.li`
   margin-left: 2rem;
+  padding-bottom: 1rem;
   list-style: none;
   a{text-decoration:none;}
   a:visited{color : #0e0e0e;}
-`
+`;
 
 export default Header;
