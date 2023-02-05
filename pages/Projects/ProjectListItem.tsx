aec96f72a9fcfc242f8fa19807bec4c91acbaf63
rdg1029 <wcle3456@gmail.com>
2023-02-05 15:32:55 +0900

    Update ProjectListItem.tsx : Apply media query at ListItemWrapper & ListItemImage

--- pages/Projects/ProjectListItem.tsx
+++ pages/Projects/ProjectListItem.tsx
@@ -20,14 +20,35 @@ const ProjectListItem = (props: Props) => {
 
 const ListItemWrapper = styled.div`
   text-align: center;
-  margin 0 40px 0 40px;
+  @media all and (min-width: 768px) {
+    margin 0 20px 0 20px;
+  }
+  margin 0 10px 0 10px;
 `;
 
 const ListItemImage = styled.img`
-  width: 560px;
-  height: 324px;
-  border-radius: 20px;
-  object-fit: cover;
+  @media all and (min-width: 768px) {
+    width: 560px;
+    height: 324px;
+    border-radius: 20px;
+    object-fit: cover;
+  }
+
+  /* 모바일 가로 & 테블릿 세로 (해상도 480px ~ 767px)*/ 
+  @media all and (min-width:480px) and (max-width:767px) {
+    width: 420px;
+    height: 243px;
+    border-radius: 20px;
+    object-fit: cover;
+  } 
+
+  /* 모바일 세로 (해상도 ~ 479px)*/ 
+  @media all and (max-width:479px) {
+    width: 280px;
+    height: 162px;
+    border-radius: 20px;
+    object-fit: cover;
+  }
 `;
 
 const ListItemTitle = styled.h2`
