fb03bd1ac4d5f4c47c180152c441fad86e250c15
rdg1029 <wcle3456@gmail.com>
2023-02-05 16:12:23 +0900

    Update ProjectDetail.tsx : Apply media query at ProjectDetailTitle

--- pages/Projects/[ProjectDetail].tsx
+++ pages/Projects/[ProjectDetail].tsx
@@ -32,7 +32,7 @@ const ProjectDetail = ()=> {
           <p>{projectData.description}</p>
         </ProjectDetailTitle>
         <ProjectDetailContent>
-          <ImageSlider width="600px" height="450px"
+          <ImageSlider
             images={projectData.image} />
           <br></br>
           
@@ -67,16 +67,81 @@ const ProjectDetailWrapper = styled.div`
 `;
 
 const ProjectDetailTitle = styled.div`
-  margin-top: 128px;
-  width: 1280px;
-  h1 {
-    text-align: left;
-    font-size: 55pt;
+  @media all and (min-width: 1280px) {
+    margin-top: 128px;
+    width: 1280px;
+    & > h1 {
+      text-align: left;
+      margin-left: 0.5rem;
+      font-size: 55pt;
+    }
+    & > p {
+      margin: 1.5rem 0 1.5rem 0.5rem;
+      font-size: 18pt;
+      font-weight: 300;
+    }
   }
-  p {
-    margin: 1.5rem 0 1.5rem 0;
-    font-size: 18pt;
-    font-weight: 300;
+
+  /* 노트북 & 테블릿 가로 (해상도 1024px ~ 1279px)*/ 
+  @media all and (min-width:1024px) and (max-width:1279px) {
+    margin-top: 128px;
+    width: 1024px;
+    & > h1 {
+      text-align: left;
+      margin-left: 0.5rem;
+      font-size: 50pt;
+    }
+    & > p {
+      margin: 1.5rem 0 1.5rem 0.5rem;
+      font-size: 16pt;
+      font-weight: 300;
+    }
+  }   
+
+  /* 테블릿 가로 (해상도 768px ~ 1023px)*/ 
+  @media all and (min-width:768px) and (max-width:1023px) {
+    margin-top: 128px;
+    width: 768px;
+    & > h1 {
+      text-align: left;
+      margin-left: 0.5rem;
+      font-size: 45pt;
+    }
+    & > p {
+      margin: 1.5rem 0 1.5rem 0.5rem;
+      font-size: 14pt;
+      font-weight: 300;
+    }
+  } 
+
+  /* 모바일 가로 & 테블릿 세로 (해상도 480px ~ 767px)*/ 
+  @media all and (min-width:480px) and (max-width:767px) {
+    margin-top: 128px;
+    width: 480px;
+    & > h1 {
+      margin-left: 0.5rem;
+      font-size: 40pt;
+    }
+    & > p {
+      margin: 1.5rem 0 1.5rem 0.5rem;
+      font-size: 12pt;
+      font-weight: 300;
+    }
+  } 
+
+  /* 모바일 세로 (해상도 ~ 479px)*/ 
+  @media all and (max-width:479px) {
+    margin-top: 128px;
+    width: 360px;
+    & > h1 {
+      margin-left: 0.5rem;
+      font-size: 35pt;
+    }
+    & > p {
+      margin: 1.5rem 0 1.5rem 0.5rem;
+      font-size: 12pt;
+      font-weight: 300;
+    }
   }
 `;
 
