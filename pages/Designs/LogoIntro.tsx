a0910a438f2ffe44c4bc74630bbba5416fc9b36c
HarenKei <seungmin1266@gmail.com>
2023-02-18 00:57:11 +0900

    Updated LogoIntro.tsx : Adjust Some Position Of Components

--- pages/Designs/LogoIntro.tsx
+++ pages/Designs/LogoIntro.tsx
@@ -3,47 +3,46 @@ import BackgroundCard from "../../src/Common/BackgroundCard";
 
 const LogoIntro = () => {
   return (
-      <LogoIntroWrapper>
-        <LogoIntroContents>
-          <LogoIntroTitle>
-            아주 짧은 고민이 만든
-            <br />
-            의미 있는 DEF:CON의 얼굴
-          </LogoIntroTitle>
-          <LogoIntroImg src="/Images/mainLogo.svg" />
-        </LogoIntroContents>
-        <LogoIntroContext>
-          <p>
-            우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.
-            <br />
-            고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.
-            <br />
-            로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고
-            <br />
-            &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를
-            입혀
-            <br />
-            단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
-          </p>
-        </LogoIntroContext>
-        <BackgroundCard
-          color="#00658F"
-          height="22rem"
-          translateX="-1000px"
-          translateY="140px"
-          type="bordered"
-        />
-      </LogoIntroWrapper>
-
+    <LogoIntroWrapper>
+      <LogoIntroContents>
+        <LogoIntroTitle>
+          아주 짧은 고민이 만든
+          <br />
+          의미 있는 DEF:CON의 얼굴
+        </LogoIntroTitle>
+        <LogoIntroImg src="/Images/mainLogo.svg" />
+      </LogoIntroContents>
+      <LogoIntroContext>
+        <p>
+          우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.
+          <br />
+          고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.
+          <br />
+          로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고
+          <br />
+          &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를 입혀
+          <br />
+          단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
+        </p>
+      </LogoIntroContext>
+      <BackgroundCard
+        color="#00658F"
+        height="22rem"
+        translateX="-1000px"
+        translateY="140px"
+        type="bordered"
+      />
+    </LogoIntroWrapper>
   );
 };
 
 const LogoIntroWrapper = styled.div`
   height: 100vh;
-  margin-bottom: 10vh;
+  margin-bottom: 5vh;
   display: flex;
   @media all and (min-width: 1280px) {
     flex-direction: row;
+    margin-bottom: 10vh;
   }
   flex-direction: column;
   justify-content: center;
@@ -54,8 +53,12 @@ const LogoIntroWrapper = styled.div`
 const LogoIntroContents = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+  @media all and (min-width: 1280px) {
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+  justify-content: center;
+  align-items: center;
 `;
 
 const LogoIntroTitle = styled.h1`
