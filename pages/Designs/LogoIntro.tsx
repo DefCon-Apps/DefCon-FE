d5e24b8c0a5723c1e76d99dd3bf8fab9756a63c6
HarenKei <seungmin1266@gmail.com>
2023-02-18 00:24:15 +0900

    Updated LogoIntro.tsx : Apply Mobile Layout At LogoIntro Section

--- pages/Designs/LogoIntro.tsx
+++ pages/Designs/LogoIntro.tsx
@@ -3,36 +3,39 @@ import BackgroundCard from "../../src/Common/BackgroundCard";
 
 const LogoIntro = () => {
   return (
-    <LogoIntroWrapper>
-      <LogoIntroContents>
-        <LogoIntroTitle>
-          아주 짧은 고민이 만든
-          <br />
-          의미 있는 DEF:CON의 얼굴
-        </LogoIntroTitle>
-        <LogoIntroImg src="/Images/mainLogo.svg" />
-      </LogoIntroContents>
-      <LogoIntroContext>
-        <p>
-          우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.
-          <br />
-          고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.
-          <br />
-          로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고
-          <br />
-          &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를 입혀
-          <br />
-          단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
-        </p>
-      </LogoIntroContext>
-      <BackgroundCard
-        color="#00658F"
-        height="22rem"
-        translateX="-1000px"
-        translateY="140px"
-        type="bordered"
-      />
-    </LogoIntroWrapper>
+    <LogoIntroContainer>
+      <LogoIntroWrapper>
+        <LogoIntroContents>
+          <LogoIntroTitle>
+            아주 짧은 고민이 만든
+            <br />
+            의미 있는 DEF:CON의 얼굴
+          </LogoIntroTitle>
+          <LogoIntroImg src="/Images/mainLogo.svg" />
+        </LogoIntroContents>
+        <LogoIntroContext>
+          <p>
+            우리들의 로고는 30분 남짓의 아주 짧은 시간에 고안되었습니다.
+            <br />
+            고등학교 짧은 점심시간을 충분히 활용한 결과라고 할 수 있죠.
+            <br />
+            로고에 사용된 메인 색상은 Windows 10의 테마의 색상에서 영감을 얻었고
+            <br />
+            &quot;DEF:CON CRAB&quot;과 세미콜론(;)은 우리의 메인 테마 컬러를
+            입혀
+            <br />
+            단조로울 수 있는 텍스트 중심의 로고에 포인트 요소가 되었습니다.
+          </p>
+        </LogoIntroContext>
+        <BackgroundCard
+          color="#00658F"
+          height="22rem"
+          translateX="-1000px"
+          translateY="140px"
+          type="bordered"
+        />
+      </LogoIntroWrapper>
+    </LogoIntroContainer>
   );
 };
 
