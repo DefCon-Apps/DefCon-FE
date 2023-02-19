5e4a4eaa5d6bd5f64114e1b1a8655bebb0b58d1f
HarenKei <seungmin1266@gmail.com>
2023-02-20 00:48:41 +0900

    Updated CrabIntro.tsx : Fix Desktop Layout Error

--- pages/Designs/CrabIntro.tsx
+++ pages/Designs/CrabIntro.tsx
@@ -17,27 +17,57 @@ const CrabIntro = () => {
   return (
     <CrabIntroWrapper>
       <CrabIntroContents>
-        <CrabIntroTitle>
-          우리의 헤리티지
-          <br />
-          DEF:CON CRAB
-        </CrabIntroTitle>
-        <CrabIntroDesc>
-          <p>
-            DEF:CON의 마스코트인 DEF:CON CRAB은 메인 컬러와 마찬가지로
-            <br />
-            Windows 10의 블루스크린에서 착안하였습니다.
-            <br />
-            DEF:CON의 철자 C의 앞에 콜론 (:)을 삽입하여 블루스크린의 이모티콘인
-            <br />
-            &quot;:(&quot;을 연상할 수 있게 만들어냈죠.
-            <br />
-            <br />
-            코딩을 하다보면 웃음을 지을 때보단 울상일 때가 더 많은 우리의 <br />
-            표정과도 비슷한 DEF:CON CRAB은 DEF:CON의 초창기 로고부터 <br />
-            유지되어온 DEF:CON의 헤리티지입니다. <br />
-          </p>
-        </CrabIntroDesc>
+        {isDesktop ? (
+          <>
+            <CrabIntroDesc>
+              <p>
+                DEF:CON의 마스코트인 DEF:CON CRAB은 메인 컬러와 마찬가지로
+                <br />
+                Windows 10의 블루스크린에서 착안하였습니다.
+                <br />
+                DEF:CON의 철자 C의 앞에 콜론 (:)을 삽입하여 블루스크린의
+                이모티콘인
+                <br />
+                &quot;:(&quot;을 연상할 수 있게 만들어냈죠.
+                <br />
+                <br />
+                코딩을 하다보면 웃음을 지을 때보단 울상일 때가 더 많은 우리의{" "}
+                <br />
+                표정과도 비슷한 DEF:CON CRAB은 DEF:CON의 초창기 로고부터 <br />
+                유지되어온 DEF:CON의 헤리티지입니다. <br />
+              </p>
+            </CrabIntroDesc>
+            <CrabIntroTitle>
+              우리의 헤리티지
+              <br />
+              DEF:CON CRAB
+            </CrabIntroTitle>
+          </>
+        ) : (
+          <>
+            <CrabIntroTitle>
+              우리의 헤리티지
+              <br />
+              DEF:CON CRAB
+            </CrabIntroTitle>
+            <CrabIntroDesc>
+              <p>
+                DEF:CON의 마스코트인 DEF:CON CRAB은 메인 컬러와 마찬가지로
+                <br />
+                Windows 10의 블루스크린에서 착안하였습니다.
+                <br />
+                DEF:CON의 철자 C의 앞에 콜론 (:)을 삽입하여 블루스크린의
+                이모티콘인 &quot;:(&quot;을 연상할 수 있게 만들어냈죠.
+                <br />
+                <br />
+                코딩을 하다보면 웃음을 지을 때보단 울상일 때가 더 많은 우리의{" "}
+                <br />
+                표정과도 비슷한 DEF:CON CRAB은 DEF:CON의 초창기 로고부터 <br />
+                유지되어온 DEF:CON의 헤리티지입니다. <br />
+              </p>
+            </CrabIntroDesc>
+          </>
+        )}
 
         <BackgroundCard
           color="#C7E7FF"
@@ -66,6 +96,9 @@ const CrabIntro = () => {
 
 const CrabIntroWrapper = styled.div`
   height: 100vh;
+  @media screen and (min-width: 1280px) {
+    margin-bottom: 10vh;
+  }
   margin-bottom: 20vh;
   display: flex;
   flex-direction: column;
@@ -78,81 +111,79 @@ const CrabIntroContents = styled.div`
   display: flex;
   @media screen and (min-width: 1280px) {
     flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CrabIntroTitle = styled.h1`
   @media screen and (min-width: 1280px) {
     font-size: 45pt;
     letter-spacing: -5px;
-    text-align: left;
+    text-align: right;
   }
-
-  @media screen and (min-width: 768px) {
-    font-size: 35pt;
-    letter-spacing: -2px;
-    text-align: center;
-  }
-
-  font-size: 25pt;
+  font-size: 30pt;
   letter-spacing: -2px;
   text-align: center;
 `;
 
 const CrabIntroDesc = styled.div`
   @media screen and (min-width: 1280px) {
-    margin-right: 14vw;
-    p {
-      font-size: 16pt;
-      font-weight: 100;
-      text-align: right;
-    }
-  }
-
-  @media screen and (min-width: 768px) {
-    p {
-      font-size: 16pt;
-      font-weight: 100;
-      text-align: right;
-    }
-  }
-
-  margin-top: 5vh;
-  p {
-    width: 80vw;
-    font-size: 12pt;
+    width: 50%;
+    margin-top: 0;
+    margin-right: 12vw;
+    font-size: 16pt;
     font-weight: 100;
-    text-align: center;
+    text-align: left;
   }
+  width: 85%;
+  margin-top: 5vh;
+  margin-right: 0;
+  font-size: 12pt;
+  font-weight: 100;
+  text-align: center;
 `;
 
 const CrabIntroLogoWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   @media screen and (min-width: 1280px) {
     flex-direction: row;
+    margin-top: 8vh;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
 
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 8vh;
+  margin-top: 5vh;
 `;
 
 const LogoHistoryWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (min-width: 1280px) {
+    margin-top: 5vh;
+    margin-right: 20vw;
+  }
   margin-top: 5vh;
+  margin-right: 0;
 `;
 
 const Logo = styled.img`
   @media screen and (min-width: 1280px) {
-    width: 10vw;
+    width: 20vw;
+    margin-bottom: 8vh;
   }
   width: 50vw;
   margin-bottom: 8vh;
+
   filter: drop-shadow(1px 1px 3px grey);
 `;
 
