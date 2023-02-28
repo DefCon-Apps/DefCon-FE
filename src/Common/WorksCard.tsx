478d446676cff8ad5ea55229a89604d621978c97
HarenKei <seungmin1266@gmail.com>
2023-02-28 18:40:31 +0900

    Updated WorksCard.tsx : Change Border-Radius Size of Works Card Components

--- src/Common/WorksCard.tsx
+++ src/Common/WorksCard.tsx
@@ -1,36 +1,35 @@
 import styled from "styled-components";
 
 interface Props {
-	text: string,
-	image: string
-};
+  text: string;
+  image: string;
+}
 
-const WorksCard = (props : Props) => {
-
-  return(
-		<WorksCardWrapper>
-			<ImageBoxStyle src={`/Images/${props.image}`}/>
-			<h1>{props.text}</h1>
-		</WorksCardWrapper>
-	);
+const WorksCard = (props: Props) => {
+  return (
+    <WorksCardWrapper>
+      <ImageBoxStyle src={`/Images/${props.image}`} />
+      <h1>{props.text}</h1>
+    </WorksCardWrapper>
+  );
 };
 
 const WorksCardWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 50px;
 
-	h1{
-		margin-top : 1rem;
-		font-size: 2.5rem;
-	}
-`
+  h1 {
+    margin-top: 1rem;
+    font-size: 2.5rem;
+  }
+`;
 
 const ImageBoxStyle = styled.img`
   width: 300px;
   height: 200px;
-	border-radius: 20px;
+  border-radius: 10px;
 `;
 
 export default WorksCard;
