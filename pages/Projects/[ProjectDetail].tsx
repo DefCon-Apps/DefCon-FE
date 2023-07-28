c4e9ee873804d40a149f42784ba2ae3347df6ef1
HarenKei <seungmin1266@gmail.com>
2023-07-28 23:03:41 +0900

    Update [ProjectDetail].tsx : Refactoring CSS Styles of ProjectDetailTitle Component

--- pages/Projects/[ProjectDetail].tsx
+++ pages/Projects/[ProjectDetail].tsx
@@ -7,28 +7,29 @@ import styled from "styled-components";
 import * as API from "../../src/Common/API";
 import ImageSlider from "../../src/Common/ImageSlider";
 
-const ProjectDetail = ()=> {
+const ProjectDetail = () => {
   const router = useRouter();
   const projectId = router.query.ProjectDetail;
   const [projectData, setProjectData] = useState<API.ProjectData>();
 
   useEffect(() => {
-    if(projectId !== undefined) {
+    if (projectId !== undefined) {
       API.getProjectData(projectId as string).then((apiResult: any) => {
         setProjectData(apiResult);
       });
     }
   }, [projectId]);
 
-  if (!projectData) return (
-    <ProjectDetailWrapper>
-      <ProjectDetailTitle>
-        <h1>Page Not Found</h1>
-      </ProjectDetailTitle>
-    </ProjectDetailWrapper>
-  );
+  if (!projectData)
+    return (
+      <ProjectDetailWrapper>
+        <ProjectDetailTitle>
+          <h1>Page Not Found</h1>
+        </ProjectDetailTitle>
+      </ProjectDetailWrapper>
+    );
 
-  return(
+  return (
     <ProjectDetailWrapper>
       <div>
         <ProjectDetailTitle>
@@ -36,26 +37,30 @@ const ProjectDetail = ()=> {
           <p>{projectData.description}</p>
         </ProjectDetailTitle>
         <ProjectDetailContent>
-          <ImageSlider
-            images={projectData.image} />
+          <ImageSlider images={projectData.image} />
           <br></br>
 
           <ReactMarkdown
             rehypePlugins={[RehypeRaw]}
-            remarkPlugins={[RemarkGFM]}>
+            remarkPlugins={[RemarkGFM]}
+          >
             {projectData.content.toString().replaceAll("\\n", "\n")}
           </ReactMarkdown>
           <br></br>
 
           <h1>Tech Stack</h1>
           {projectData.tech.map((item, i) => {
-            return <p key={i}>{item}</p>
+            return <p key={i}>{item}</p>;
           })}
           <br></br>
 
           <h1>Role</h1>
           {projectData.user.map((item, i) => {
-            return <p key={i}><b>{item.user}</b> - {item.role}</p>
+            return (
+              <p key={i}>
+                <b>{item.user}</b> - {item.role}
+              </p>
+            );
           })}
           <br></br>
 
@@ -65,91 +70,109 @@ const ProjectDetail = ()=> {
         </ProjectDetailContent>
       </div>
     </ProjectDetailWrapper>
-  )
-}
+  );
+};
 
 const ProjectDetailWrapper = styled.div`
   font-family: "Noto Sans KR";
   min-height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
+
+  & > div {
+    width: 1030px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20vh;
+  }
 `;
 
 const ProjectDetailTitle = styled.div`
-  margin-top: 128px;
+  margin-top: 200px;
+  margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
   @media all and (min-width: 1280px) {
-    width: 1280px;
+    width: 100%;
     & > h1 {
+      width: 100%;
       text-align: left;
-      margin-left: 0.5rem;
-      font-size: 55pt;
+      font-size: 3.5em;
     }
     & > p {
-      margin: 1.5rem 0 1.5rem 0.5rem;
-      font-size: 18pt;
+      margin-top: 1rem;
+      font-size: 1.5rem;
       font-weight: 300;
     }
   }
 
-  /* 노트북 & 테블릿 가로 (해상도 1024px ~ 1279px)*/ 
-  @media all and (min-width:1024px) and (max-width:1279px) {
-    width: 1024px;
+  /* 노트북 & 테블릿 가로 (해상도 1024px ~ 1279px)*/
+  @media all and (min-width: 1024px) and (max-width: 1279px) {
+    width: 700px;
     & > h1 {
+      width: 100%;
       text-align: left;
-      margin-left: 0.5rem;
-      font-size: 50pt;
+      font-size: 2.8rem;
     }
+
     & > p {
-      margin: 1.5rem 0 1.5rem 0.5rem;
-      font-size: 16pt;
+      margin-top: 1rem;
+      font-size: 1.313em;
       font-weight: 300;
     }
-  }   
+  }
 
-  /* 테블릿 가로 (해상도 768px ~ 1023px)*/ 
-  @media all and (min-width:768px) and (max-width:1023px) {
-    width: 768px;
+  /* 테블릿 가로 (해상도 768px ~ 1023px)*/
+  @media all and (min-width: 768px) and (max-width: 1023px) {
+    width: 500px;
+    align-items: center;
     & > h1 {
+      width: 100%;
       text-align: center;
-      margin-left: 0.5rem;
-      font-size: 45pt;
+      font-size: 3.5em;
     }
     & > p {
       text-align: center;
-      margin: 1.5rem 0 1.5rem 0.5rem;
-      font-size: 14pt;
+      margin-top: 1rem;
+      font-size: 1.313em;
       font-weight: 300;
     }
-  } 
+  }
 
-  /* 모바일 가로 & 테블릿 세로 (해상도 480px ~ 767px)*/ 
-  @media all and (min-width:480px) and (max-width:767px) {
-    width: 480px;
+  /* 모바일 가로 & 테블릿 세로 (해상도 480px ~ 767px)*/
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    width: 400px;
+    align-items: center;
     & > h1 {
-      text-alitn: center;
-      margin-left: 0.5rem;
-      font-size: 40pt;
+      text-align: center;
+      font-size: 3.2em;
     }
     & > p {
       text-align: center;
-      margin: 1.5rem 0 1.5rem 0.5rem;
-      font-size: 12pt;
+      margin-top: 1rem;
+      font-size: 1.17rem;
       font-weight: 300;
     }
-  } 
+  }
 
-  /* 모바일 세로 (해상도 ~ 479px)*/ 
-  @media all and (max-width:479px) {
-    width: 360px;
+  /* 모바일 세로 (해상도 ~ 479px)*/
+  @media all and (max-width: 479px) {
+    width: 400px;
+    align-items: center;
     & > h1 {
-      text-alitn: center;
-      margin-left: 0.5rem;
-      font-size: 35pt;
+      text-align: center;
+      font-size: 2.9em;
     }
     & > p {
       text-align: center;
       margin: 1.5rem 0 1.5rem 0.5rem;
-      font-size: 12pt;
+      font-size: 1.17em;
       font-weight: 300;
     }
   }
