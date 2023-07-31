0d8d1ce7ecbfb94f3c10acb26ed771e2906e0aee
HarenKei <seungmin1266@gmail.com>
2023-07-31 18:40:39 +0900

    Update Designs.tsx : Add Flex Styled For Desings Page Layout

--- pages/Designs/Designs.tsx
+++ pages/Designs/Designs.tsx
@@ -1,13 +1,22 @@
 import LogoIntro from "./LogoIntro";
 import CrabIntro from "./CrabIntro";
+import styled from "styled-components";
 
 const Designs = () => {
   return (
-    <>
+    <DesignStyle>
       <LogoIntro />
       <CrabIntro />
-    </>
+    </DesignStyle>
   );
 };
+
+const DesignStyle = styled.div`
+  font-family: "Noto Sans KR";
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Designs;
