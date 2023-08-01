f99b63cd36920971a4383cdfe06c8521800724ee
rdg1029 <wcle3456@gmail.com>
2023-08-02 00:06:08 +0900

    Update ProjectSection.tsx : Edit section width

--- pages/Projects/ProjectSection.tsx
+++ pages/Projects/ProjectSection.tsx
@@ -27,7 +27,7 @@ const SectionWrapper = styled.div`
 
   @media all and (min-width: 1280px) {
     & > div {
-      width: 1400px;
+      width: 1030px;
     }
     
     h1 {
@@ -46,7 +46,7 @@ const SectionWrapper = styled.div`
   /* 노트북 & 테블릿 가로 (해상도 1024px ~ 1279px)*/ 
   @media all and (min-width:1024px) and (max-width:1279px) {
     & > div {
-      width: 1024px;
+      width: 700px;
     }
     
     h1 {
@@ -65,7 +65,7 @@ const SectionWrapper = styled.div`
   /* 테블릿 가로 (해상도 768px ~ 1023px)*/ 
   @media all and (min-width:768px) and (max-width:1023px) {
     & > div {
-      width: 768px;
+      width: 500px;
     }
     
     h1 {
@@ -84,7 +84,7 @@ const SectionWrapper = styled.div`
   /* 모바일 가로 & 테블릿 세로 (해상도 480px ~ 767px)*/ 
   @media all and (min-width:480px) and (max-width:767px) {
     & > div {
-      width: 480px;
+      width: 400px;
     }
     
     h1 {
@@ -103,7 +103,7 @@ const SectionWrapper = styled.div`
   /* 모바일 세로 (해상도 ~ 479px)*/ 
   @media all and (max-width:479px) {
     & > div {
-      width: 360px;
+      width: 400px;
     }
     
     h1 {
