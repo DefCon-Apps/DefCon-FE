fa6243a89c7c8a9485996112cae7a4db4230f083
HarenKei <seungmin1266@gmail.com>
2023-08-02 01:35:11 +0900

    Update index.tsx : delete Next/Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
 import Main from "./Main/Main"
 
 const Home = () => {
   return (
     <>
-        <Head>
-            <title>Def:Con - 대학생 프로그래밍 팀</title>
-        </Head>
       <Main/>
     </>
   )
